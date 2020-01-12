4c85ad70cde4621651986ba63f5eb031c8115562
Joe Gelay <joegelay@gmail.com>
2020-01-12 17:15:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,47 @@
 def using_push
   
 end
+
+def using_unshift
+  
+end
+
+def using_pop
+  
+end
+
+def using_pop_with_args
+  
+end
+
+def using_shift
+  
+end
+
+def using_shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
